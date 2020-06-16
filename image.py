a2f1a964834fe04dd2a1492d3258ce9ff4bfc7a8
harshitgarg22 <hg1229@gmail.com>
2020-06-16 17:44:03 +0530

    Pixedlated Grayscale Image generator

---
+++ image.py
@@ -0,0 +1,12 @@
+from PIL import Image
+import numpy as np
+
+def random_img(output, width, height):
+
+    array = np.random.randint(0,255, (height,width,2))  
+    array = np.array(array, dtype=np.uint8)
+    img = Image.fromarray(array)
+    img.save(output)
+
+
+random_img('random.png', 4, 4)
